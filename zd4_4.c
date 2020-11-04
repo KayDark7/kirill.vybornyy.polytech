2ce77e97c25177279c1bbdde6e03f895cdddc391
KayDark7 <71705297+KayDark7@users.noreply.github.com>
2020-11-04 18:09:23 +0300

    Update zd4_4.c

--- zd4_4.c
+++ zd4_4.c
@@ -5,17 +5,17 @@ int main()
 	setlocale(LC_ALL, "Rus");
 int a, b, s, r, p;
 float c;
-printf("Ââåäèòå a\n");
+printf("Введите a\n");
 scanf("%d", &a);
-printf("Ââåäèòå b\n");
+printf("Введите b\n");
 scanf("%d", &b);
 s=(a*a)+(b*b);
 r=(a*a)-(b*b);
 p=(a*a)*(b*b);
 c=((float)(a*a))/((float)(b*b));
-printf("Ñóììà = %d \n", s);
-printf("Ðàçíîñòü = %d \n", r);
-printf("Ïðîèçâåäåíèå = %d \n", p);
-printf("×àñòíîå = %f \n", c);
+printf("Сумма = %d \n", s);
+printf("Разность = %d \n", r);
+printf("Произведение = %d \n", p);
+printf("Разность = %f \n", c);
 return 0;
 }
