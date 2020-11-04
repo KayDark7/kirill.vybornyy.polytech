8b03a437b098c45531f4657028e3f500a1d68807
KayDark7 <71705297+KayDark7@users.noreply.github.com>
2020-11-04 22:12:41 +0300

    Update zd5_4.c

--- zd5_4.c
+++ zd5_4.c
@@ -5,17 +5,17 @@ int main()
 {
 int x1,y1,x2,y2;
 float S,P;
-printf("Координаты вершины 1\n x = ");
+printf("РљРѕРѕСЂРґРёРЅР°С‚С‹ РІРµСЂС€РёРЅС‹ 1\n x = ");
 scanf("%d",&x1);
 printf(" y = ");
 scanf("%d",&y1);
-printf("Координаты вершины 2\n x = ");
+printf("РљРѕРѕСЂРґРёРЅР°С‚С‹ РІРµСЂС€РёРЅС‹ 2\n x = ");
 scanf("%d",&x2);
 printf(" y = ");
 scanf("%d",&y2);
 S = abs(x1-x2) * abs(y1-y2);
 P = 2*(abs(x1-x2)+(y1-y2));
-printf ("Площадь = %f\n", S);
-printf ("Периметр = %f\n", P);
+printf ("РџР»РѕС‰Р°РґСЊ = %f\n", S);
+printf ("РџРµСЂРёРјРµС‚СЂ = %f\n", P);
 return 0; 
 }
