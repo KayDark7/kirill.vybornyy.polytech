76aa77045ae887c30757dc263635b0070a268420
KayDark7 <71705297+KayDark7@users.noreply.github.com>
2020-11-04 22:12:14 +0300

    Update zd5_3.c

--- zd5_3.c
+++ zd5_3.c
@@ -4,23 +4,23 @@ int main()
 {
 int x,y,x1,y1,x2,y2;
 float d,d1,p;
-printf("Координаты точки A\n x = ");
+printf("РљРѕРѕСЂРґРёРЅР°С‚С‹ С‚РѕС‡РєРё A\n x = ");
 scanf("%d",&x);
 printf(" y = ");
 scanf("%d",&y);
-printf("Координаты точки B\n x = ");
+printf("РљРѕРѕСЂРґРёРЅР°С‚С‹ С‚РѕС‡РєРё B\n x = ");
 scanf("%d",&x1);
 printf(" y = ");
 scanf("%d",&y1);
-printf("Координаты точки C\n x = ");
+printf("РљРѕРѕСЂРґРёРЅР°С‚С‹ С‚РѕС‡РєРё C\n x = ");
 scanf("%d",&x2);
 printf(" y = ");
 scanf("%d",&y2);
 d = sqrt((x2-x)*(x2-x)+(y2-y)*(y2-y));
 d1 = sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
 p = d*d1;
-printf ("Отрезок AC = %f\n", d);
-printf ("Отрезок BC = %f\n", d1);
-printf ("Произведение длин отрезков = %f\n", p);
+printf ("РћС‚СЂРµР·РѕРє AC = %f\n", d);
+printf ("РћС‚СЂРµР·РѕРє BC = %f\n", d1);
+printf ("РџСЂРѕРёР·РІРµРґРµРЅРёРµ РґР»РёРЅ РѕС‚СЂРµР·РєРѕРІ = %f\n", p);
 return 0; 
 }
